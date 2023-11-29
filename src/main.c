717e1a3095ce2c99e5835462aa756e0f673e408b
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-11-29 18:04:29 +0100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "stdint.h"
+#include "stdio.h"
+#include <stdint.h>
+
+typedef double f64;
+typedef int32_t i32;
+
+i32
+main (i32 argc, char *argv[])
+{
+  printf ("Test done\n");
+  return 0;
+}
