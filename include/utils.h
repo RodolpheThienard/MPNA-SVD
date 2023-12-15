90a30b7db2b642a1db2795187469742c95b0b664
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-12-15 15:30:45 +0100

    Update ALLOC macro

--- include/utils.h
+++ include/utils.h
@@ -1,30 +1,31 @@
+#include <cblas.h>
 #include <lapacke.h>
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <cblas.h>
 #include <time.h>
-
 
 typedef double f64;
 typedef int32_t i32;
 typedef uint64_t u64;
 typedef size_t usize;
 
-
-#define ALLOC(X, Y)                                                           \
+#define ALLOC(X, n)                                                           \
   do                                                                          \
-  {                                                                           \
-    Y = malloc (X);                                                           \
-    memset (Y, 0, X);                                                         \
-  }                                                                           \
+    {                                                                         \
+      X = malloc (sizeof (*X) * n);                                           \
+      if (!X)                                                                 \
+        {                                                                     \
+          perror ("malloc");                                                  \
+          exit (1);                                                           \
+        }                                                                     \
+      memset (X, 0, sizeof (*X) * n);                                         \
+    }                                                                         \
   while (0)
 
-void transposeMatrix(f64* matrix, i32 rows, i32 cols);
-void affiche_mat (i32 m, i32 n, f64 *mat); 
+void transposeMatrix (f64 *matrix, i32 rows, i32 cols);
+void affiche_mat (i32 m, i32 n, f64 *mat);
 u64 rdtsc ();
-void test_matrice(f64 *, i32, i32);
-void test_matrice_v1(f64 *M, i32 m, i32 n);
-
-
+void test_matrice (f64 *, i32, i32);
+void test_matrice_v1 (f64 *M, i32 m, i32 n);
--- src/main.c
+++ src/main.c
@@ -12,66 +12,61 @@ main (i32 argc, char *argv[])
   // definition
   i32 size_m = atoi (argv[1]);
   i32 size_n = atoi (argv[2]);
-  f64 *matrix_a, *matrix_u1,*matrix_u2,*matrix_c,*matrix_b1,*matrix_b2, *matrix_v1,*matrix_v2,  *eval_v, *eval, 
-      *sigma, *res, *res2, *matrix_ut;
+  f64 *matrix_a, *matrix_u1, *matrix_u2, *matrix_c, *matrix_b1, *matrix_b2,
+      *matrix_v1, *matrix_v2, *eval_v, *eval, *sigma, *res, *res2, *matrix_ut;
 
   // matrix allocation
-  ALLOC (sizeof (f64) * size_m * size_n, matrix_a);
-  ALLOC (sizeof (f64) * size_m * size_n, matrix_u1);
-  ALLOC (sizeof (f64) * size_m * size_n, matrix_u2);
-  ALLOC (sizeof (f64) * size_n * size_n, matrix_c);
-  ALLOC (sizeof (f64) * size_n * size_n, matrix_b1);
-  ALLOC (sizeof (f64) * size_n * size_n, matrix_b2);
-  ALLOC (sizeof (f64) * size_n * size_n, matrix_v1);
-  ALLOC (sizeof (f64) * size_n * size_n, matrix_v2);
-  ALLOC (sizeof (f64) * size_n, eval);
-  ALLOC (sizeof (f64) * size_n, eval_v);
-  ALLOC (sizeof (f64) * size_m * size_n, sigma);
-  ALLOC (sizeof (f64) * size_m * size_n, res);
-  ALLOC (sizeof (f64) * size_m * size_n, res2);
-
+  ALLOC (matrix_a, size_m * size_n);
+  ALLOC (matrix_u1, size_m * size_n);
+  ALLOC (matrix_u2, size_m * size_n);
+  ALLOC (matrix_b1, size_n * size_n);
+  ALLOC (matrix_b2, size_n * size_n);
+  ALLOC (matrix_v1, size_n * size_n);
+  ALLOC (matrix_v2, size_n * size_n);
+  ALLOC (matrix_c, size_n * size_n);
+  ALLOC (eval, size_n);
+  ALLOC (eval_v, size_n);
+  ALLOC (sigma, size_m * size_n);
+  ALLOC (res, size_m * size_n);
+  ALLOC (res2, size_m * size_n);
 
   test_matrice (matrix_a, size_m, size_n);
 
-
-
   // Initialisation vecteur v1
-  test_matrice_v1(matrix_v1, size_n, size_n);
+  test_matrice_v1 (matrix_v1, size_n, size_n);
 
-  #ifdef DEBUG
+#ifdef DEBUG
   printf ("Matrice A\n");
   affiche_mat (size_m, size_n, matrix_a);
-  #endif
+#endif
 
   // Bidiagonalisation de C = U1*AV1
-  bidiagonalisation (matrix_a, matrix_c, matrix_u1, matrix_v1,
-                   size_m, size_n);
+  bidiagonalisation (matrix_a, matrix_c, matrix_u1, matrix_v1, size_m, size_n);
 
-  #ifdef DEBUG
+#ifdef DEBUG
   printf ("Matrice U1\n");
   affiche_mat (size_m, size_n, matrix_u1);
   printf ("Matrice V1\n");
   affiche_mat (size_n, size_n, matrix_v1);
   printf ("Matrice C\n");
   affiche_mat (size_n, size_n, matrix_c);
-  #endif
+#endif
 
-
- 
-  //Cacul du SVD de C = U2 SIGMA V2*
+  // Cacul du SVD de C = U2 SIGMA V2*
 
   // Compute U2 = vecteur propre de B1=CCt
   cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasTrans, size_n, size_n, size_n,
-               1.0, matrix_c, size_n, matrix_c, size_n, 0.0, matrix_b1, size_n);
+               1.0, matrix_c, size_n, matrix_c, size_n, 0.0, matrix_b1,
+               size_n);
   qr_method (matrix_b1, eval, matrix_u2, size_n);
-
 
   // Compute V2 = vecteur propre de B2=CtC
   cblas_dgemm (CblasRowMajor, CblasTrans, CblasNoTrans, size_n, size_n, size_n,
-               1.0, matrix_c, size_n, matrix_c, size_n, 0.0, matrix_b2, size_n);
+               1.0, matrix_c, size_n, matrix_c, size_n, 0.0, matrix_b2,
+               size_n);
   qr_method (matrix_b2, eval_v, matrix_v2, size_n);
 
-  //Sigma final 
+  // Sigma final
   for (int i = 0; i < size_n; i++)
     {
       f64 eval_i = eval[i];
@@ -84,17 +79,15 @@ main (i32 argc, char *argv[])
           sigma[i * size_m + i] = 0.0;
         }
     }
-    transposeMatrix (sigma, size_n, size_m);
-    #ifdef DEBUG
-   // printf ("Matrice Sigma\n");
-    //affiche_mat (size_m, size_n, sigma);
-        printf ("Matrice U2\n");
-    affiche_mat (size_n, size_n, matrix_u2);
-    printf ("Matrice V2\n");
-    affiche_mat (size_n, size_n, matrix_v2);
-    #endif
-
-  
+  transposeMatrix (sigma, size_n, size_m);
+#ifdef DEBUG
+  // printf ("Matrice Sigma\n");
+  // affiche_mat (size_m, size_n, sigma);
+  printf ("Matrice U2\n");
+  affiche_mat (size_n, size_n, matrix_u2);
+  printf ("Matrice V2\n");
+  affiche_mat (size_n, size_n, matrix_v2);
+#endif
 
   // Beginning of measurement
 #ifdef BENCHMARK
