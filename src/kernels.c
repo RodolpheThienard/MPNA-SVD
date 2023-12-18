c1ba005db046ad879c5c5404f3ce0c6c2c5f3cf6
RodolpheThienard <thienard.rodolphe1@gmail.com>
2023-12-18 10:41:56 +0100

    Gram schmidt & dgemm Updating gram shcmidt function by removing all cblas functions. implementation of our dgemm

--- src/kernels.c
+++ src/kernels.c
@@ -2,6 +2,19 @@
 #include <cblas.h>
 #include <math.h>
 
+void
+dgemm (double *C, double *A, double *B, int n)
+{
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      {
+        double r = 0;
+        for (int k = 0; k < n; k++)
+          r += A[i * n + k] * B[k * n + j];
+        C[i * n + j] = r;
+      }
+}
+
 /* Bidiagonalization of Golub-Kahan-Lanczos
    B = U * AV
    INPUT : allocated matrix a,b,u,v
@@ -14,7 +27,8 @@ bidiagonalisation (f64 *matrix_a, f64 *matrix_b, f64 *matrix_u, f64 *matrix_v,
                    usize m, usize n)
 {
 
-  cblas_dscal (n, 1.0 /column_euclidean_norm(matrix_v, n, n, 0), &matrix_v[0],
+  cblas_dscal (n, 1.0 / column_euclidean_norm (matrix_v, n, n, 0),
+               &matrix_v[0],
                n); // On normalise le vecteur v1 initialisée au hasard
 
   for (usize i = 0; i < n; i++)
@@ -24,94 +38,104 @@ bidiagonalisation (f64 *matrix_a, f64 *matrix_b, f64 *matrix_u, f64 *matrix_v,
           matrix_u[j * n + i]
               = cblas_ddot (n, &matrix_a[j * n], 1, &matrix_v[i], n);
 
-       
           if (i == 0)
             continue;
 
-      //Probleme à cette étape, à la n-1 itération, avant cette opération la n-1 colonne de u à des valeur différente de 0 
-      //alors que après cette opération elle deviens systématiquement nul alors que matrix_b[(i - 1) * n + i]et
-      // matrix_u[j * n + (i - 1)] sont non nul
+          // Probleme à cette étape, à la n-1 itération, avant cette opération
+          // la n-1 colonne de u à des valeur différente de 0 alors que après
+          // cette opération elle deviens systématiquement nul alors que
+          // matrix_b[(i - 1) * n + i]et
+          //  matrix_u[j * n + (i - 1)] sont non nul
           matrix_u[j * n + i]
               -= matrix_b[(i - 1) * n + i] * matrix_u[j * n + (i - 1)];
-      }
+        }
 
-      matrix_b[i * n + i] = column_euclidean_norm(matrix_u, m, n, i);
+      matrix_b[i * n + i] = column_euclidean_norm (matrix_u, m, n, i);
 
-      
-      
       for (usize j = 0; j < m; j++)
         matrix_u[j * n + i] /= matrix_b[i * n + i];
 
       if (i == n - 1)
         continue;
-      
-      for (usize j = 0; j < n; j++)
-      {
-          for (usize k = 0; k < m; k++)
-            {
-                matrix_v[j * n + i+1]  += matrix_a[k*n + j] * matrix_u[k * n + i];
-            }
-        matrix_v[j * n + i + 1] -= matrix_b[i * n + i] * matrix_v[j * n + i];
-      }
-      matrix_b[i * n + (i + 1)] = column_euclidean_norm(matrix_v, n, n, i+1);
-
-  
-
-     
-      for (usize j = 0; j < n; j++)
-        matrix_v[j * n + i + 1] /= matrix_b[i * n + (i + 1)];
-
-    }
-}
-
-/* TODO
- */
-void
-gram_schmidt_modified (f64 *Q, f64 *R, f64 * matrix_a, usize n)
-{
-  for (usize i = 0; i < n; i++)
-    {
-      for (usize j = 0; j < n; j++)
-        Q[j * n + i] = (i == j) ? 1.0 : 0.0;
 
       for (usize j = 0; j < n; j++)
         {
-          R[j * n + i] = cblas_ddot (n, Q + j * n, 1, Q + i * n, 1);
-
-          for (usize k = 0; k < n; k++)
-            Q[k * n + i] -= R[j * n + i] * Q[k * n + j];
+          for (usize k = 0; k < m; k++)
+            {
+              matrix_v[j * n + i + 1]
+                  += matrix_a[k * n + j] * matrix_u[k * n + i];
+            }
+          matrix_v[j * n + i + 1] -= matrix_b[i * n + i] * matrix_v[j * n + i];
         }
-      R[i * n + i] = cblas_dnrm2 (n, Q + i * n, 1);
+      matrix_b[i * n + (i + 1)]
+          = column_euclidean_norm (matrix_v, n, n, i + 1);
 
       for (usize j = 0; j < n; j++)
-        Q[j * n + i] /= R[i * n + i];
+        matrix_v[j * n + i + 1] /= matrix_b[i * n + (i + 1)];
+    }
+}
+
+void
+modifiedGramSchmidt (f64 *A, f64 *Q, f64 *R, usize m, usize n)
+{
+  for (int k = 0; k < m; ++k)
+    {
+      // Calcul de R
+      for (int j = 0; j < k; ++j)
+        {
+          double dotProduct = 0.0;
+          for (int i = 0; i < n; ++i)
+            {
+              dotProduct += Q[i * n + j] * A[i * m + k];
+            }
+          R[j * m + k] = dotProduct;
+          for (int i = 0; i < n; ++i)
+            {
+              A[i * m + k] -= R[j * n + k] * Q[i * n + j];
+            }
+        }
+
+      // Normalisation et mise à jour de Q
+      double norm = 0.0;
+      for (int i = 0; i < n; ++i)
+        {
+          norm += A[i * m + k] * A[i * m + k];
+        }
+      norm = sqrt (norm);
+
+      R[k * m + k] = norm;
+
+      for (int i = 0; i < n; ++i)
+        {
+          Q[i * n + k] = A[i * m + k] / norm;
+        }
     }
 }
 
 /* Written in courses */
 void
-gram_schmidt_modified_2 (double *a, double *q, int n)
+gram_schmidt_modified (f64 *a, f64 *q, f64 *r, usize n)
 {
-  double *r = calloc (n * n, sizeof (double));
-  double *w = calloc (n, sizeof (double));
-  for (int k = 0; k < n; k++)
+  f64 *w;
+  ALLOC (w, n);
+  for (usize k = 0; k < n; k++)
     {
       cblas_dcopy (n, &a[k * n], 1, w, 1);
-      for (int j = 0; j < k; j++)
+      for (usize j = 0; j < k; j++)
         {
           r[k * n + j] = cblas_ddot (n, &q[j * n], 1, w, 1);
-          double *rjk_qj = calloc (n, sizeof (double));
+          f64 *rjk_qj;
+          ALLOC (rjk_qj, n);
           cblas_daxpy (n, r[k * n + j], &q[j * n], 1, rjk_qj, 1);
           cblas_daxpy (n, -1, rjk_qj, 1, w, 1);
           free (rjk_qj);
         }
       r[k * n + k] = cblas_dnrm2 (n, w, 1);
-      for (int i = 0; i < n; i++)
+      for (usize i = 0; i < n; i++)
         {
           q[k * n] = w[i] / r[k * n + k];
         }
     }
-  free (r);
   free (w);
 }
 
@@ -135,7 +159,8 @@ gershgorin_test (f64 *A, usize n)
 /* TODO
  */
 void
-qr_method (f64 *matrix_a, f64 *eigen_values, f64 *eigen_vectors, usize n)
+qr_method (f64 *matrix_a, f64 *eigen_values, f64 *eigen_vectors, usize m,
+           usize n)
 {
   f64 *Q, *R;
   ALLOC (Q, n * n);
@@ -143,14 +168,18 @@ qr_method (f64 *matrix_a, f64 *eigen_values, f64 *eigen_vectors, usize n)
 
   for (usize i = 0; i < ITERMAX; i++)
     {
-      gram_schmidt_modified (Q, R, matrix_a, n);
+      // gram_schmidt_modified (matrix_a, Q, R, n);
+      modifiedGramSchmidt (matrix_a, Q, R, m, n);
 
-      cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1, Q, n,
-                   R, n, 1, matrix_a, n);
+      dgemm (matrix_a, R, Q, n);
+      // cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1, Q,
+      // n,
+      // R, n, 1, matrix_a, n);
 
       if (gershgorin_test (matrix_a, n) < ERR)
         break;
     }
+
   cblas_dcopy (n, R, n + 1, eigen_values, 1);
   cblas_dcopy (n, Q, n, eigen_vectors, n);
 
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "../include/kernels.h"
 #include "../include/utils.h"
 #include <cblas-openblas.h>
 #include <cblas.h>
@@ -40,6 +41,11 @@ main (i32 argc, char *argv[])
   affiche_mat (size_m, size_n, matrix_a);
 #endif
 
+  // Beginning of measurement
+#ifdef BENCHMARK
+  u64 t0 = rdtsc ();
+#endif
+
   // Bidiagonalisation de C = U1*AV1
   bidiagonalisation (matrix_a, matrix_c, matrix_u1, matrix_v1, size_m, size_n);
 
@@ -57,13 +63,13 @@ main (i32 argc, char *argv[])
   cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasTrans, size_n, size_n, size_n,
                1.0, matrix_c, size_n, matrix_c, size_n, 0.0, matrix_b1,
                size_n);
-  qr_method (matrix_b1, eval, matrix_u2, size_n);
+  qr_method (matrix_b1, eval, matrix_u2, size_m, size_n);
 
   // Compute V2 = vecteur propre de B2=CtC
   cblas_dgemm (CblasRowMajor, CblasTrans, CblasNoTrans, size_n, size_n, size_n,
                1.0, matrix_c, size_n, matrix_c, size_n, 0.0, matrix_b2,
                size_n);
-  qr_method (matrix_b2, eval_v, matrix_v2, size_n);
+  qr_method (matrix_b2, eval_v, matrix_v2, size_m, size_n);
 
   // Sigma final
   for (int i = 0; i < size_n; i++)
@@ -78,7 +84,7 @@ main (i32 argc, char *argv[])
           sigma[i * size_m + i] = 0.0;
         }
     }
-  transposeMatrix (sigma, size_n, size_m);
+    // transposeMatrix (sigma, size_n, size_m);
 #ifdef DEBUG
   // printf ("Matrice Sigma\n");
   // affiche_mat (size_m, size_n, sigma);
@@ -86,11 +92,6 @@ main (i32 argc, char *argv[])
   affiche_mat (size_n, size_n, matrix_u2);
   printf ("Matrice V2\n");
   affiche_mat (size_n, size_n, matrix_v2);
-#endif
-
-  // Beginning of measurement
-#ifdef BENCHMARK
-  u64 t0 = rdtsc ();
 #endif
 
   for (int i = 0; i < size_m; i++)
@@ -109,28 +110,9 @@ main (i32 argc, char *argv[])
 #ifdef DEBUG
   printf ("Matrice sigma\n");
   affiche_mat (size_m, size_n, sigma);
-#endif 
-  /*cblas_dgemm (CblasRowMajor, CblasTrans, CblasTrans, size_m, size_n, size_n,
-               1.0, evec, size_m, sigma, size_n, 0.0, res, size_m);
-
-  i32 info_v = LAPACKE_dsyev (LAPACK_COL_MAJOR, 'V', 'U', size_n, matrix_v,
-                              size_n, eval);
-  // Vérification des erreurs
-  if (info_v > 0)
-    {
-      fprintf (stderr, "Erreur dans le calcul des valeurs propres\n");
-      return 1;
-    }
-
-  // transpose evec matrix_vt
-  transposeMatrix (matrix_v, size_m, size_n);
-
-#ifdef DEBUG
-  printf ("Matrice V*\n");
-  affiche_mat (size_n, size_n, matrix_v);
 #endif
-*/
-// End of measurement + print
+
+  // End of measurement + print
 #ifdef BENCHMARK
   u64 t1 = rdtsc ();
   printf ("total cycles : %ld\n", t1 - t0);
